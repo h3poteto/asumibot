8914dc768494552205520bb1a7268c44ba92d35d
h3poteto <h3.poteto@gmail.com>
2013-09-24 23:41:30 +0900

    add twitter shell script

---
+++ script/twitter_follow.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/akira/projects/asumibot
+rake twitter:follower RAILS_ENV=production
---
+++ script/twitter_reply.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/akira/projects/asumibot
+rake twitter:reply RAILS_ENV=production
