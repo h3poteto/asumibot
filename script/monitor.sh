fb040f6e84565ff4332fe0abd8c6f874e4daf1ca
h3poteto <h3.poteto@gmail.com>
2013-10-12 00:51:42 +0900

    add monitor script for restart userstream

---
+++ script/monitor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+while true
+do
+    sleep 10
+    isAlive=`ps -ef | grep "userstream" | grep -v grep | wc -l`
+    if [ $isAlive -lt 2 ]; then
+	#死んでる
+	cd /home/akira/projects/asumibot/
+	nohup sh ./script/userstream.sh > log/stream.log 2> log/stream_err.log < /dev/null &
+    fi
+done
