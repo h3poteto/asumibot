3e311ffb9c241a1b26e1d26e84c9304cda4d7a9c
h3poteto <h3.poteto@gmail.com>
2013-12-08 18:49:40 +0900

    edit after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admins_serifs_path
+    end
+  end
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
