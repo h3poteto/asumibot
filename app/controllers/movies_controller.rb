90e5f4d5ddf04f85d85816a36904babfa879c1fe
h3poteto <h3.poteto@gmail.com>
2014-01-29 23:40:00 +0900

    add movie search

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,37 @@
+class MoviesController < ApplicationController
+  layout "user"
+  # GET /movies
+  # GET /movies.json
+  def index
+
+    @search_niconico = NiconicoMovie.search(:title_or_url_or_description_cont => params[:search])
+    @search_youtube = YoutubeMovie.search(:title_or_url_or_description_cont => params[:search])
+    @niconico = @search_niconico.result.where(disabled: false).order("id DESC")
+    @youtube = @search_youtube.result.where(disabled: false).order("id DESC")
+    @movies = @niconico + @youtube
+    @movies.sort! do |a, b|
+       b.created_at <=> a.created_at
+    end
+
+    
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @movies }
+    end
+  end
+
+  # GET /movies/1
+  # GET /movies/1.json
+  def show_youtube
+    @youtube = YoutubeMovie.find(params[:id])
+    s = @youtube.url.index("?v=")
+    e = @youtube.url.index("&")
+    @id = @youtube.url[s+3..e-1]
+  end
+  def show_niconico
+    @niconico = NiconicoMovie.find(params[:id])
+    s = @niconico.url.index("/watch/")
+    @id = @niconico.url[s+7..100]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Asumibot::Application.routes.draw do
   resources :patients, only: [:show, :index]
   resources :recommends, only: [:index]
+  resources :movies, only: [:index]
+  match "/movies/show_niconico/:id" => "movies#show_niconico", :as => :niconico
+  match "/movies/show_youtube/:id" => "movies#show_youtube", :as => :youtube
 
   devise_for :admins
 
