44f2292c356bc7cd3e9452e5aeef90967f28e1b6
h3poteto <h3.poteto@gmail.com>
2014-01-29 23:58:00 +0900

    update recommends for movie_show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def movie_show_url(movie)
+    if movie.kind_of?(YoutubeMovie) || movie.kind_of?(TodayYoutube)
+      return youtube_path(movie)
+    elsif movie.kind_of?(NiconicoMovie) || movie.kind_of?(TodayNiconico)
+      return niconico_path(movie)
+    end
+  end
 end
