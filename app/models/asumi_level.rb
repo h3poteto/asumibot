ea1621f68949d82b901bc568a4557c72ecc9d793
h3poteto <h3.poteto@gmail.com>
2015-02-24 09:25:42 +0900

    fixes ranking calcurate for asumi level

--- app/models/asumi_level.rb
+++ app/models/asumi_level.rb
@@ -14,8 +14,8 @@ class AsumiLevel < ActiveRecord::Base
       month_asumi = 0
       asumi_levels = AsumiLevel.where(patient_id: p.id).where(created_at: from..to)
       asumi_levels.each do |l|
-        month_tweet += l.tweet_count
-        month_asumi += l.asumi_count
+        month_tweet += l.tweet_count.present? ? l.tweet_count : 0
+        month_asumi += l.asumi_count.present? ? l.asumi_count : 0
       end
       month_level = month_asumi * 100 / month_tweet if month_tweet != 0 && month_tweet > 20
       rankings.push(user_info.new(p.id, p.name, month_level))
