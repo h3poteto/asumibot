8639bae5cae3b3f1f8a1b8d5a0cab28d36c07d9f
h3poteto <h3.poteto@gmail.com>
2015-04-20 01:25:45 +0900

    closes #7 unicornのプロセス起動設定を修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,14 +16,16 @@ preload_app true
 timeout 45
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{ server.config[:pid] }.oldbin"
-  unless old_pid == server.pid
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
     begin
-      # SIGTTOU だと worker_processes が多いときおかしい気がする
-      Process.kill :QUIT, File.read(old_pid).to_i
+      Process.kill("WINCH", File.read(old_pid).to_i)
+      Thread.new {
+        sleep 30
+        Process.kill("KILL", File.read(old_pid).to_i)
+      }
     rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
     end
   end
 end
