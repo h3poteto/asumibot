b1bd0f6ae78afe581d0fb844bf80956c7cdade15
h3poteto <h3.poteto@gmail.com>
2015-08-25 02:13:24 +0900

    closes #33 絵文字保存時に適当にエンコードしておく

--- app/models/asumi_tweet.rb
+++ app/models/asumi_tweet.rb
@@ -1,3 +1,13 @@
 class AsumiTweet < ActiveRecord::Base
   belongs_to :patient
+
+  before_save :encode_emoji
+
+  def encode_emoji
+    self.tweet = utf8mb4_encode_numericentity(self.tweet)
+  end
+
+  def utf8mb4_encode_numericentity(str)
+    str.gsub(/[^\u{0}-\u{FFFF}]/) { '&#x%X;' % $&.ord }
+  end
 end
