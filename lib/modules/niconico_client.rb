1b5cfea7a1c1fb9234511e4b7bb09045f6ab78bd
h3poteto <h3.poteto@gmail.com>
2016-04-03 18:44:03 +0900

    refs #43 [rubocop]ハッシュインデントを調節

--- lib/modules/niconico_client.rb
+++ lib/modules/niconico_client.rb
@@ -10,17 +10,22 @@ class NiconicoClient
 
 
   def get_today_movies()
-
     @niconico_search.each do |keyword|
-      nico = NicoSearchSnapshot.new('asumibot')
-      results = nico.search(keyword, search: [:title, :description, :tags], size: 19, filters: [
-        {type: :range,
-          field: :start_time,
-          from: Time.current.yesterday.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S"),
-          to: Time.current.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S"),
-          include_upper: false
-        }
-      ])
+      nico = NicoSearchSnapshot.new("asumibot")
+      results = nico.search(
+        keyword,
+        search: [:title, :description, :tags],
+        size: 19,
+        filters: [
+          {
+            type: :range,
+            field: :start_time,
+            from: Time.current.yesterday.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S"),
+            to: Time.current.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S"),
+            include_upper: false
+          }
+        ]
+      )
       results.each do |r|
         add_niconicos(r)
         add_today_niconicos(r)
@@ -30,10 +35,14 @@ class NiconicoClient
 
   def get_popular_movies
     @niconico_tags.each do |tag|
-
-      nico = NicoSearchSnapshot.new('asumibot')
+      nico = NicoSearchSnapshot.new("asumibot")
       # 現状sizeで20以上指定をすると返却値のエンコードがおかしくなる
-      results = nico.search(tag, search: [:tags_exact], sort_by: :last_comment_time, size: 15)
+      results = nico.search(
+        tag,
+        search: [:tags_exact],
+        sort_by: :last_comment_time,
+        size: 15
+      )
 
       results.each_with_index do |r, i|
         add_niconicos(r)
@@ -43,16 +52,32 @@ class NiconicoClient
   end
 
   private
+
   # 本当はdbとしてユニーク制約を張っておきたいが，現状はモデルのバリデータでユニーク制約を担保できている
   def add_popular_niconicos(movie, priority)
-    NiconicoPopular.create(title: Sanitize.clean(movie.title), url: @watch_path + movie.cmsid, description: Sanitize.clean(movie.description), priority: priority )
+    NiconicoPopular.create(
+      title: Sanitize.clean(movie.title),
+      url: @watch_path + movie.cmsid,
+      description: Sanitize.clean(movie.description),
+      priority: priority
+    )
   end
 
   def add_today_niconicos(movie)
-    TodayNiconico.create(title: Sanitize.clean(movie.title), url: @watch_path + movie.cmsid, description: Sanitize.clean(movie.description), priority: nil)
+    TodayNiconico.create(
+      title: Sanitize.clean(movie.title),
+      url: @watch_path + movie.cmsid,
+      description: Sanitize.clean(movie.description),
+      priority: nil
+    )
   end
 
   def add_niconicos(movie)
-    NiconicoMovie.create(title: Sanitize.clean(movie.title), url: @watch_path + movie.cmsid, description: Sanitize.clean(movie.description), priority: nil )
+    NiconicoMovie.create(
+      title: Sanitize.clean(movie.title),
+      url: @watch_path + movie.cmsid,
+      description: Sanitize.clean(movie.description),
+      priority: nil
+    )
   end
 end
