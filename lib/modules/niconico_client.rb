821cd4915fef6317ded1bec29c58b6b0352a5634
h3poteto <h3.poteto@gmail.com>
2017-03-19 12:18:12 +0900

    refs #73 Add NiconicoSearch for snapshot api v2

--- lib/modules/niconico_client.rb
+++ lib/modules/niconico_client.rb
@@ -1,4 +1,3 @@
-require 'nico_search_snapshot'
 require 'sanitize'
 
 class NiconicoClient
@@ -11,21 +10,17 @@ class NiconicoClient
 
   def get_today_movies()
     @niconico_search.each do |keyword|
-      nico = NicoSearchSnapshot.new("asumibot")
+      nico = NiconicoSearch.new("asumibot")
       results = nico.search(
-        keyword,
-        search: [:title, :description, :tags],
-        size: 19,
-        filters: [
-          {
-            type: :range,
-            field: :start_time,
-            from: Time.current.yesterday.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S"),
-            to: Time.current.beginning_of_day.strftime("%Y-%m-%d %H:%M:%S"),
-            include_upper: false
-          }
-        ]
+        query: keyword,
+        targets: [:title, :description, :tags],
+        options: {
+          _limit: 50,
+          "filters[start_time][gte]" => Time.current.yesterday.beginning_of_day.strftime("%Y-%m-%dT%H:%M:%S%:z"),
+          "filters[start_time][lte]" => Time.current.beginning_of_day.strftime("%Y-%m-%dT%H:%M:%S%:z")
+        }
       )
+
       results.each do |r|
         add_niconicos(r)
         add_today_niconicos(r)
@@ -35,13 +30,14 @@ class NiconicoClient
 
   def get_popular_movies
     @niconico_tags.each do |tag|
-      nico = NicoSearchSnapshot.new("asumibot")
-      # 現状sizeで20以上指定をすると返却値のエンコードがおかしくなる
+      nico = NiconicoSearch.new("asumibot")
       results = nico.search(
-        tag,
-        search: [:tags_exact],
-        sort_by: :last_comment_time,
-        size: 15
+        query: tag,
+        targets: [:tags],
+        options: {
+          _limit: 50,
+          _sort: :lastCommentTime
+        }
       )
 
       results.each_with_index do |r, i|
@@ -58,7 +54,7 @@ class NiconicoClient
   def add_popular_niconicos(movie, priority)
     NiconicoPopular.create(
       title: Sanitize.clean(movie.title),
-      url: @watch_path + movie.cmsid,
+      url: @watch_path + movie.contentId,
       description: Sanitize.clean(movie.description),
       priority: priority
     )
@@ -67,7 +63,7 @@ class NiconicoClient
   def add_today_niconicos(movie)
     TodayNiconico.create(
       title: Sanitize.clean(movie.title),
-      url: @watch_path + movie.cmsid,
+      url: @watch_path + movie.contentId,
       description: Sanitize.clean(movie.description),
       priority: nil
     )
@@ -76,7 +72,7 @@ class NiconicoClient
   def add_niconicos(movie)
     NiconicoMovie.create(
       title: Sanitize.clean(movie.title),
-      url: @watch_path + movie.cmsid,
+      url: @watch_path + movie.contentId,
       description: Sanitize.clean(movie.description),
       priority: nil
     )
