c9aeebabacd016b477d6af32cd2a35d29d96a526
h3poteto <h3.poteto@gmail.com>
2017-08-04 21:49:20 +0900

    #87 Remove test directory

--- test/unit/new_serif_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NewSerifTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/niconico_popular_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NiconicoPopularTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
